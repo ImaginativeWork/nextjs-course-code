fb35e6aa2f5591d8cbed79545ad74a0092fce330
Doug Franklin <dfranklin@copt.com>
2024-02-03 06:44:46 -0500

    Nested page added

---
+++ pages/portfolio/_list.js
@@ -0,0 +1,10 @@
+const ListNoFolder = () => {
+
+  return (
+    <>
+      <h1>ListNoFolder(pages/portfolio/list.js, Video Tutorial)</h1>
+    </>
+  )
+}
+
+export default ListNoFolder;
---
+++ pages/portfolio/list/index.js
@@ -0,0 +1,11 @@
+const ListWithFolder = () => {
+
+  return (
+    <>
+      <h1>ListWithFolder (pages/portfolio/list/index.js Video Tutorial)</h1>
+      <div>If list.js appears in the parent of this folder, then this won't be seen.</div>
+    </>
+  )
+}
+
+export default ListWithFolder;
